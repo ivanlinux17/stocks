85fa2b73dd21b0f9b9e195fc7de3008191a4bae5
Ivan Rodriguez <ivan_rodriguez@xyratex.com>
2015-03-10 14:41:52 +0000

    Added a comment

--- command.py
+++ command.py
@@ -1,5 +1,5 @@
 from Calc import Calc
 
-print "This is version 4.X and the library version: "
+print "This is version 4.X (bug fixed MDW-1) and the library version: "
 calc = Calc ()
 calc.version()
